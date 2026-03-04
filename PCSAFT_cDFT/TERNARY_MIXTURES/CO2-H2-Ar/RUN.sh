5b473a89f767a7dcdaf1e336c7187535819e63e0
D.Raju <d.raju@tudelft.nl>
2026-03-04 17:21:41 +0100

    Add BINARY_INTERACTION_PARAMETERS as submodule

---
+++ PCSAFT_cDFT/TERNARY_MIXTURES/CO2-H2-Ar/RUN.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=cDFT_TERNARY-(CO2-H2-Ar)
+#SBATCH --partition=serial
+#SBATCH --time=6:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+
+start_time=$(date +"%T")
+echo "Job started at: $start_time"
+
+source  /home/darshan/A6/py_A6/bin/activate
+export PATH=$HOME/texlive/2025/bin/x86_64-linux:$PATH
+
+which python
+which latex
+
+papermill VLE_IFT.ipynb VLE_IFT_output.ipynb
---
+++ PCSAFT_cDFT/thermoift/__init__.py
@@ -0,0 +1,3 @@
+from . import PLOT_SETTINGS
+from . import FeosPlugin
+from .BINARY_INTERACTION_PARAMETERS.KIJ import BinaryInteractions as KIJ
