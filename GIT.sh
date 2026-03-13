3e7febf6a8746a8df4babcacef334fe66580a9f1
D.Raju <d.raju@tudelft.nl>
2026-03-13 19:38:48 +0100

    added semi-emperical correlations to compute IFT from feos

--- GIT.sh
+++ GIT.sh
@@ -2,6 +2,6 @@
 
 # git init
 git add *
-git commit -m "ML -PySR model"
+git commit -m "added semi-emperical correlations to compute IFT from feos"
 # git remote add origin https://github.com/Darz2/A6.git
 git push -u origin master
